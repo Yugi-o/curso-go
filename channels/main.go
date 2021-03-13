d1f2095c8ec935498e7cb580d1f56e15c035f4e3
Jonathan García <69655589+Yugi-o@users.noreply.github.com>
2021-03-12 23:58:50 -0600

    channels

---
+++ channels/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	canal1 := make(chan time.Duration)
+	go bucle(canal1)
+	fmt.Println("Llegue hasta aqui")
+
+	msg := <-canal1
+	fmt.Println(msg)
+}
+
+func bucle(canal chan time.Duration) {
+	inicio := time.Now()
+	for i := 0; i < 1000000000; i++ {
+
+	}
+	final := time.Now()
+	canal <- final.Sub(inicio)
+}
